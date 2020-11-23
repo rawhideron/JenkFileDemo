ba25d8e8f0b9ad28241984195562cfae2400730a
Ron Goodman <rawhideron@gmail.com>
2020-11-23 13:49:47 -0500

    Jfile

---
+++ src/test/java/com/rawhide/Jfile/JfileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rawhide.Jfile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JfileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
